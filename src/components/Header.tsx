eb2f731bd230f1490e300180c31eb34baf921803
Stefan Larsen <stefan.larsen@northstake.dk>
2024-09-03 17:26:56 +0200

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,34 @@
 'use client'
-import { useState } from 'react';
-import Link from 'next/link';
-import AboutModal from './AboutModal';
+import { useState } from 'react'
+import Link from 'next/link'
+import AboutModal from './AboutModal'
+import Image from 'next/image'
 
 export default function Header() {
-  const [isAboutModalOpen, setIsAboutModalOpen] = useState(false);
+  const [isAboutModalOpen, setIsAboutModalOpen] = useState(false)
 
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold">
-          BlobHook
+    <header className='bg-gray-800 text-white p-4'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <Link href='/'>
+          <div className='flex items-center cursor-pointer'>
+            <Image
+              src='/mascot_notext.png' // Path to your mascot image
+              alt='BlobHook Logo'
+              width={40} // Adjust the width as needed
+              height={40} // Adjust the height as needed
+              className='mr-3'
+            />
+            <span className='text-2xl font-bold'>
+              <span className='text-white-400'>Blob</span>
+              <span className='text-blue-400'>Hook</span>
+            </span>
+          </div>
         </Link>
         <nav>
-          <ul className="flex space-x-4">
-    
+          <ul className='flex space-x-4'>
             <li>
-              <button
-                onClick={() => setIsAboutModalOpen(true)}
-                className="hover:text-gray-300"
-              >
+              <button onClick={() => setIsAboutModalOpen(true)} className='hover:text-gray-300'>
                 About
               </button>
             </li>
@@ -28,5 +37,5 @@ export default function Header() {
       </div>
       <AboutModal isOpen={isAboutModalOpen} onClose={() => setIsAboutModalOpen(false)} />
     </header>
-  );
+  )
 }
