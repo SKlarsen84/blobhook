ea950502176b333b512b33930b8574c7f6912c04
Stefan Larsen <stefan.larsen@northstake.dk>
2024-09-03 19:36:33 +0200

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,8 +8,8 @@ export default function Header() {
   const [isAboutModalOpen, setIsAboutModalOpen] = useState(false)
 
   return (
-    <header className='bg-gray-800 text-white p-4'>
-      <div className='container mx-auto flex justify-between items-center'>
+    <header className='bg-gray-900 text-white p-4'>
+      <div className='container mx-auto flex items-center justify-between ml-0'>
         <Link href='/'>
           <div className='flex items-center cursor-pointer'>
             <Image
