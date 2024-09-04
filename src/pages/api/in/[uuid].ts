8607cd7a617935245cee785d2b25dc6b9f70800e
Stefan Larsen <stefan.larsen@northstake.dk>
2024-09-04 13:45:15 +0200

    check in

--- src/pages/api/in/[uuid].ts
+++ src/pages/api/in/[uuid].ts
@@ -1,29 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import admin, { initializeApp } from 'firebase-admin'
-import { cert } from 'firebase-admin/app'
+import admin from 'firebase-admin'
+import { cert, initializeApp } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL
-    })
-  })
-}
-
-// Initialize Firebase Admin SDK
-const app = initializeApp({
-  credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
-  })
-})
-
-const db = getFirestore(app)
+import { db } from '@/lib/firebaseAdmin'
 
 const messaging = admin.messaging()
 
@@ -36,16 +16,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   try {
     await db.collection('requests').add({
-      uuid,
+      uuid: uuid,
+      webhook_url: uuid,
+      method: req.method,
       headers: req.headers,
       body: req.body,
-      method: req.method,
-      timestamp: new Date().toISOString(),
-      ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-      responseCode: res.statusCode
+      timestamp: new Date().toISOString()
     })
 
-/*     //get all subscriptions from the subscriptions collection for this uuid.
+    //get all subscriptions from the subscriptions collection for this uuid.
     const subscriptionSnapshot = await db.collection('subscriptions').where('uuid', '==', uuid).get()
     const subscriptions = subscriptionSnapshot.docs.map(doc => doc.data())
 
@@ -61,7 +40,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       }
 
       await messaging.sendEachForMulticast(message)
-    } */
+    }
 
     res.status(200).json({ message: 'Request received' })
   } catch (error) {
